18fba9928f2bf6a7f1aaf1553313388f3cf5da00
Zi Wang <zi.wang@hcl.com>
2023-02-03 05:10:58 -0800

    adding template

---
+++ includes/constants.js
@@ -0,0 +1,15 @@
+ const SOURCE_DATASET = "fivetran_log_copy";
+ const SOURCE_PROJECT = "sample1-328718";
+ const fivetran_log_using_sync_alert_messages = true
+ const fivetran_log_using_transformations = false
+ const fivetran_log_using_triggers = true
+ const fivetran_log_using_account_membership = true
+ 
+ module.exports = { 
+     SOURCE_DATASET, 
+     SOURCE_PROJECT, 
+     fivetran_log_using_sync_alert_messages, 
+     fivetran_log_using_transformations, 
+     fivetran_log_using_triggers, 
+     fivetran_log_using_account_membership 
+};
