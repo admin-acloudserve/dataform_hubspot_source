2c9b78cefca6279480c855b617ce0c252ccefc4e
Zi Wang <zi.wang@hcl.com>
2023-02-04 21:30:48 -0800

    get company columns

--- includes/get_company_columns.sql
+++ includes/get_company_columns.sql
@@ -1,23 +1,21 @@
-{% macro get_company_columns() %}
+function get_company_column() {
 
-{% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
-    {"name": "id", "datatype": dbt.type_int()},
-    {"name": "is_deleted", "datatype": "boolean"},
-    {"name": "property_name", "datatype": dbt.type_string()},
-    {"name": "property_description", "datatype": dbt.type_string()},
-    {"name": "property_createdate", "datatype": dbt.type_timestamp()},
-    {"name": "property_industry", "datatype": dbt.type_string()},
-    {"name": "property_address", "datatype": dbt.type_string()},
-    {"name": "property_address_2", "datatype": dbt.type_string()},
-    {"name": "property_city", "datatype": dbt.type_string()},
-    {"name": "property_state", "datatype": dbt.type_string()},
-    {"name": "property_country", "datatype": dbt.type_string()},
-    {"name": "property_annualrevenue", "datatype": dbt.type_int()}
-] %}
+    columns = [
+        {"name": "_fivetran_synced", "datatype": "TIMESTAMP"},
+        {"name": "id", "datatype": "INT64"},
+        {"name": "is_deleted", "datatype": "boolean"},
+        {"name": "property_name", "datatype": "STRING"},
+        {"name": "property_description", "datatype": "STRING"},
+        {"name": "property_createdate", "datatype": "TIMESTAMP"},
+        {"name": "property_industry", "datatype": "STRING"},
+        {"name": "property_address", "datatype": "STRING"},
+        {"name": "property_address_2", "datatype": "STRING"},
+        {"name": "property_city", "datatype": "STRING"},
+        {"name": "property_state", "datatype": "STRING"},
+        {"name": "property_country", "datatype": "STRING"},
+        {"name": "property_annualrevenue", "datatype": "INT64"}
+    ]
 
-{{ fivetran_utils.add_pass_through_columns(columns, var('hubspot__company_pass_through_columns')) }}
 
-{{ return(columns) }}
-
-{% endmacro %}
+    return columns
+}
