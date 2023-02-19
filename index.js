d93aee1b594810053184dc585cc27140f8baca83
ADMIN ADMIN <admin@acloudserve.com>
2023-02-19 23:45:29 +0000

    test output

--- index.js
+++ index.js
@@ -1,24 +1,96 @@
-// TODO: import files from includes
-// const file_one = require("./includes/file_one");
-// const file_two = require("./includes/file_two");
+const SOURCE_DATASET = "hubspot_copy";
+const SOURCE_PROJECT = "sample1-328718";
+const hubspot_sales_enabled = true
+const hubspot_company_enabled = true
+const hubspot_marketing_enabled = true
+const hubspot_contact_list_member_enabled = true
+const hubspot_contact_list_enabled = true
+const hubspot_contact_merge_audit_enabled = true
+const hubspot_contact_property_enabled = true
+const hubspot_contact_enabled = false
+const hubspot__company_pass_through_columns = []
+const hubspot__contact_pass_through_columns = []
+const hubspot__deal_pass_through_columns = []
+const hubspot__ticket_pass_through_columns = []
+const hubspot_deal_enabled = true
+const hubspot_deal_company_enabled = true
+const hubspot_deal_contact_enabled = true
+const hubspot_email_event_enabled = true
+const hubspot_email_event_bounce_enabled = true
+const hubspot_email_event_click_enabled = true
+const hubspot_email_event_deferred_enabled = true
+const hubspot_email_event_delivered_enabled = true
+const hubspot_email_event_dropped_enabled = true
+const hubspot_email_event_forward_enabled = true
+const hubspot_email_event_open_enabled = true
+const hubspot_email_event_print_enabled = true
+const hubspot_email_event_sent_enabled = true
+const hubspot_email_event_spam_report_enabled = true
+const hubspot_email_event_status_change_enabled = true
+const hubspot_engagement_enabled = true
+const hubspot_engagement_call_enabled = true
+const hubspot_engagement_company_enabled = true
+const hubspot_engagement_contact_enabled = true
+const hubspot_engagement_deal_enabled = true
+const hubspot_engagement_email_enabled = true
+const hubspot_engagement_meeting_enabled = true
+const hubspot_engagement_note_enabled = true
+const hubspot_engagement_task_enabled = true
+const hubspot_owner_enabled = true
+const hubspot_service_enabled = true
+const hubspot_ticket_deal_enabled = true
+const hubspot__pass_through_all_columns = false
+const test_output = ref('stg_hubspot__email_event_bounce_tmp')
 
-const stg_hubspot__company_property_history_tmp = require("./definitions/models//tmp/stg_hubspot__company_property_history_tmp");
 
-module.exports = (params) => {
 
-    params = {
-      // TODO: set default params
 
-      ...params
-    };
 
-    // Publish and return datasets.
-    let result = {
-    // TODO: update files to call with params
-    //   file_one: file_one(params),
-    //   file_two: file_two(params)
-      stg_hubspot__company_property_history_tmp: stg_hubspot__company_property_history_tmp(),
-    };
 
-    return result;
-}
+ 
+ module.exports = { 
+     SOURCE_DATASET, 
+     SOURCE_PROJECT,
+     hubspot_sales_enabled,
+     hubspot_company_enabled,
+     hubspot_marketing_enabled,
+     hubspot_contact_list_member_enabled,
+     hubspot_contact_list_enabled,
+     hubspot_contact_merge_audit_enabled,
+     hubspot_contact_property_enabled,
+     hubspot_contact_enabled,
+     hubspot__company_pass_through_columns,
+     hubspot_deal_enabled,
+     hubspot_deal_company_enabled,
+     hubspot_deal_contact_enabled,
+     hubspot_email_event_enabled,
+     hubspot_email_event_bounce_enabled,
+     hubspot_email_event_click_enabled,
+     hubspot_email_event_deferred_enabled,
+     hubspot_email_event_delivered_enabled,
+     hubspot_email_event_dropped_enabled,
+     hubspot_email_event_forward_enabled,
+     hubspot_email_event_open_enabled,
+     hubspot_email_event_print_enabled,
+     hubspot_email_event_sent_enabled,
+     hubspot_email_event_spam_report_enabled,
+     hubspot_email_event_status_change_enabled,
+     hubspot_engagement_enabled,
+     hubspot_engagement_call_enabled,
+     hubspot_engagement_company_enabled,
+     hubspot_engagement_contact_enabled,
+     hubspot_engagement_deal_enabled,
+     hubspot_engagement_email_enabled,
+     hubspot_engagement_meeting_enabled,
+     hubspot_engagement_note_enabled,
+     hubspot_engagement_task_enabled,
+     hubspot_owner_enabled,
+     hubspot_service_enabled,
+     hubspot_ticket_deal_enabled,
+     hubspot__pass_through_all_columns,
+     hubspot__contact_pass_through_columns,
+     hubspot__deal_pass_through_columns,
+     hubspot__ticket_pass_through_columns,
+     test_output
+
+};
